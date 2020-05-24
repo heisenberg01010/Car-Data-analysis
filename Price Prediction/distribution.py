06ac4e8d29a7a3ba66161ab19674128cd9e62592
heisenberg01010 <jailodha01@gmail.com>
2020-05-24 17:45:56 +0530

    error percentage

--- Price Prediction/distribution.py
+++ Price Prediction/distribution.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -10,12 +11,16 @@ df = pd.read_csv (r'/home/heisenberg/Downloads/CarPrice_Assignment.csv')
 X = df[['enginesize']]
 Y = df['price']
 lm.fit(X, Y)
-print(lm.score(X, Y))
-print(lm.intercept_)
-print(lm.coef_)
+
+print("Intercept : "  ,lm.intercept_)
+print("coefficient : " ,lm.coef_)
 
 Yhat = lm.predict(X)
-print(Yhat[0:5])
+print(Yhat)
+
+print("Mean Squared Error : "  ,mean_squared_error(Y, Yhat))
+print("R squared error : " ,lm.score(X, Y))
+
 
 ax1 = sns.distplot(df['price'], hist = False, color = "r", label = "Actual value")
 sns.distplot(Yhat, hist = False, color = "b", label = "Fitted Value", ax = ax1)
